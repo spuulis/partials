c91969f589f5450e7a31c2eb82b3e85ff0c4626c
Viesturs Spulis <v.spuulis@gmail.com>
2020-01-08 21:02:37 +0200

    Added simple axis to canvas (which visualizes the sound wave)

--- scripts/graph.js
+++ scripts/graph.js
@@ -3,11 +3,30 @@ var c, ctx;
 function onLoad() {
     c = document.getElementById("graph");
     ctx = c.getContext("2d");
+    drawAxis();
+}
+
+function drawAxis() {
+    ctx.strokeStyle = "#999999";
+
+    for(let lin = 0; lin < 3; lin++) {
+        ctx.beginPath();
+        ctx.moveTo(0, 200 * lin + 125);
+        ctx.lineTo(800, 200 * lin + 125);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(1, 200 * lin + 30);
+        ctx.lineTo(1, 200 * lin + 220);
+        ctx.stroke();
+    }
 }
 
 function drawTone(tone) {
     ctx.clearRect(0, 0, 800, 700);
+    drawAxis();
     for(let lin = 0; lin < 3; lin++) {
+        ctx.strokeStyle = "#000000"
         ctx.beginPath();
         ctx.moveTo(0, 200 * lin + 125 + tone[0] * 100);
         
